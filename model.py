2d35668d7992119d89eeb3db1db228ff8497c2eb
mehdi cherti <mehdi@cherti.name>
2016-05-14 21:32:25 +0200

    update

--- model.py
+++ model.py
@@ -6,7 +6,8 @@ def build_dense(w=32, h=32, c=1,
 				nb_hidden=100,
 				nb_outputs=10,
                 nb_blocks=3,
-                layer_per_block=3):
+                layer_per_block=3,
+                dropout_p=0.5):
     l_in = layers.InputLayer((None, c, w, h), name="input")
     hids = []
     l_hid = layers.DenseLayer(l_in, nb_hidden, nonlinearity=rectify, name="hid0")
@@ -16,7 +17,45 @@ def build_dense(w=32, h=32, c=1,
     	l_alpha = layers.DenseLayer(l_hid_block_first, 1, nonlinearity=sigmoid, name="alpha{}".format(i))
     	for j in range(1, layer_per_block + 1):
         	l_hid = layers.DenseLayer(l_hid, nb_hidden, nonlinearity=rectify, name="hid[{}, {}]".format(i, j))
+        	if dropout_p > 0:
+        		l_hid = layers.DropoutLayer(l_hid, dropout_p)
         	hids.append(l_hid)
+        l_hid = Gate((l_hid_block_first, l_hid, l_alpha), name="gate{}".format(i)) 
+    l_out = layers.DenseLayer(l_hid, nb_outputs, nonlinearity=softmax, name="output")
+    return l_out
+
+def build_conv(w=32, h=32, c=1,
+				nb_filters=32,
+				filter_size=5,
+				filter_size_first=None,
+				nb_outputs=10,
+                nb_blocks=3,
+                layer_per_block=3,
+                dropout_p=0):
+	l_in = layers.InputLayer((None, c, w, h), name="input")
+
+	if filter_size_first is None:
+		filter_size_first = filter_size
+	hids = []
+	l_hid = layers.Conv2DLayer(
+            l_in,
+            num_filters=nb_filters,
+            filter_size=(filter_size_first, filter_size_first),
+            nonlinearity=rectify,
+            name="hid0")
+    for i in range(1, nb_blocks + 1):
+    	l_hid_block_first = l_hid
+    	l_alpha = layers.DenseLayer(l_hid_block_first, 1, nonlinearity=sigmoid, name="alpha{}".format(i))
+    	for j in range(1, layer_per_block + 1):
+			l_hid = layers.Conv2DLayer(
+	            l_in,
+	            num_filters=nb_filters,
+	            filter_size=(filter_size, filter_size),
+	            nonlinearity=rectify,
+	            name="hid[{}, {}]".format(i, j))        	
+			if dropout_p > 0:
+	        	l_hid = layers.DropoutLayer(l_hid, dropout_p)
+	        hids.append(l_hid)
         l_hid = Gate((l_hid_block_first, l_hid, l_alpha), name="gate{}".format(i)) 
     l_out = layers.DenseLayer(l_hid, nb_outputs, nonlinearity=softmax, name="output")
     return l_out
--- tasks.py
+++ tasks.py
@@ -1,7 +1,7 @@
 from invoke import task
 from collections import OrderedDict
 
-from model import build_dense
+from model import build_dense, build_conv
 import theano
 import theano.tensor as T
 from lasagne import layers, objectives, updates
@@ -21,20 +21,33 @@ def train():
 
 	X = T.tensor4()
 	y = T.ivector()
-	net = build_dense(
-		w=w, h=w, c=c, 
-		nb_hidden=50, nb_outputs=10, 
-		nb_blocks=4, layer_per_block=3)
+	#net = build_dense(
+	#	w=w, h=w, c=c, 
+	#	nb_hidden=50, nb_outputs=10, 
+	#	nb_blocks=4, layer_per_block=3)
+
+	net = build_conv(
+		w=w, h=h, c=c,
+		nb_filters=16,
+		filter_size=5,
+		nb_outputs=10,
+		nb_blocks=3,
+		layer_per_block=3
+	)
 	
 	print('Compiling the net...')
 
 	y_pred = layers.get_output(net, X)
-	y_acc = T.eq(y_pred.argmax(axis=1), y).mean()
+	y_pred_detm = layers.get_output(net, X, deterministic=True)
 	#predict_fn = theano.function([X], y_pred)
-	loss = objectives.categorical_crossentropy(y_pred, y)
-	loss = loss.mean()
-	loss_fn = theano.function([X, y], loss)
-	acc_fn = theano.function([X, y], y_acc)
+	
+	loss = objectives.categorical_crossentropy(y_pred, y).mean()
+
+	loss_detm = objectives.categorical_crossentropy(y_pred, y).mean()
+	y_acc_detm = T.eq(y_pred_detm.argmax(axis=1), y).mean()
+
+	loss_fn = theano.function([X, y], loss_detm)
+	acc_fn = theano.function([X, y], y_acc_detm)
 
 	params = layers.get_all_params(net, trainable=True)
 	grad_updates = updates.momentum(loss, params, learning_rate=learning_rate, momentum=momentum)
@@ -49,8 +62,8 @@ def train():
 	train = MNIST(which='train')
 	train.load()
 	train.X = preprocess(train.X)
-	train.X = train.X[0:128*10]
-	train.y = train.y[0:128*10]
+	#train.X = train.X[0:128*100]
+	#train.y = train.y[0:128*100]
 
 	test = MNIST(which='test')
 	test.load()
