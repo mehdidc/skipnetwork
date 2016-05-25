d2c4f67a1600dd396f17485488eac0d019fc1eae
mehdi cherti <mehdi@cherti.name>
2016-05-25 15:58:33 +0200

    update

--- helpers.py
+++ helpers.py
@@ -10,7 +10,7 @@ def iterate_minibatches(inputs, targets, batchsize, shuffle=False):
     if shuffle:
         indices = np.arange(len(inputs))
         np.random.shuffle(indices)
-    for start_idx in range(0, len(inputs) - batchsize + 1, batchsize):
+    for start_idx in range(0, max(len(inputs) - batchsize + 1, len(inputs)), batchsize):
         if shuffle:
             excerpt = indices[start_idx:start_idx + batchsize]
         else:
--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 from lasagne import layers, init
 from lasagne.nonlinearities import (linear, sigmoid, rectify, very_leaky_rectify, softmax, tanh)
 from lasagne.layers import BatchNormLayer
+from sample import SimpleBernoulliSampleLayer
 import theano.tensor as T
 
 def tanh_lecun(x):
@@ -41,6 +42,7 @@ def build_dense(w=32, h=32, c=1,
 				nb_outputs=10,
                 nb_blocks=3,
                 layer_per_block=3,
+                sample_alpha=False,
                 dropout_p=0):
     l_in = layers.InputLayer((None, c, w, h), name="input")
     hids = []
@@ -50,7 +52,9 @@ def build_dense(w=32, h=32, c=1,
 
     for i in range(1, nb_blocks + 1):
     	l_hid_block_first = l_hid
-    	l_alpha = layers.DenseLayer(l_hid_block_first, 1, nonlinearity=sigmoid, name="alpha{}".format(i))
+    	l_alpha = layers.DenseLayer(l_hid_block_first, 1, nonlinearity=sigmoid, name="alpha{}".format(i), b=init.Constant(-2.))
+    	if sample_alpha:
+    		l_alpha = SimpleBernoulliSampleLayer(l_alpha)
     	for j in range(1, layer_per_block + 1):
         	l_hid = layers.DenseLayer(l_hid, nb_hidden, nonlinearity=rectify, W=init.HeUniform(gain='relu'), name="hid[{}, {}]".format(i, j))
         	if dropout_p > 0:
@@ -102,6 +106,7 @@ def build_conv(w=32, h=32, c=1,
 				nb_filters=32,
 				filter_size=5,
 				filter_size_first=None,
+				sample_alpha=False,
 				nb_outputs=10,
                 nb_blocks=3,
                 layer_per_block=3,
@@ -119,9 +124,15 @@ def build_conv(w=32, h=32, c=1,
             nonlinearity=rectify,
             name="hid0")
 	for i in range(1, nb_blocks + 1):
+
 		l_hid_block_first = l_hid
-		l_alpha = layers.DenseLayer(l_hid_block_first, 1, nonlinearity=sigmoid, name="alpha{}".format(i))
+		l_alpha = layers.DenseLayer(l_hid_block_first, 1, nonlinearity=sigmoid, b=init.Constant(-2.), name="alpha{}".format(i))
+    	if sample_alpha:
+    		l_alpha = SimpleBernoulliSampleLayer(l_alpha)
+	
 		for j in range(1, layer_per_block + 1):
+			#if j == layer_per_block and pool:
+			#	nb_filters_ *= 2
 			l_hid = layers.Conv2DLayer(
 	            l_hid,
 	            num_filters=nb_filters,
@@ -132,11 +143,15 @@ def build_conv(w=32, h=32, c=1,
 	            name="hid[{}, {}]".format(i, j))        	
 			if dropout_p > 0:
 				l_hid = layers.DropoutLayer(l_hid, dropout_p)
+			print(l_hid.output_shape)
 			hids.append(l_hid)
+		print(l_hid_block_first.output_shape, l_hid.output_shape)
 		l_hid = Gate((l_hid_block_first, l_hid, l_alpha), name="gate{}".format(i)) 
 		if pool:
 			l_hid = layers.MaxPool2DLayer(l_hid, pool_size=(2, 2))
-			filter_size *= 2
+			#nb_filters *= 2
+
+
 	l_hid = layers.GlobalPoolLayer(l_hid)
 	l_out = layers.DenseLayer(l_hid, nb_outputs, nonlinearity=softmax, W=init.HeUniform(gain='relu'), name="output")
 	return l_out
